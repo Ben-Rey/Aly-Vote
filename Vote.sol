5086c55af75bd5b3452578811b0ff1105811fe5c
Benjamin Reynes <benjamin@morio.co>
2022-06-07 19:28:10 +0200

    Setup

---
+++ Vote.sol
@@ -0,0 +1,34 @@
+// SPDX-License-Identifier: MIT
+
+pragma solidity 0.8.14;
+
+import "https://github.com/OpenZeppelin/openzeppelin-contracts/blob/master/contracts/access/Ownable.sol";
+
+contract Voting {
+    uint256 winningProposalId; // id du gagnant
+
+    struct Voter {
+        bool isRegistered;
+        bool hasVoted;
+        uint256 votedProposalId;
+    }
+
+    struct Proposal {
+        string description;
+        uint256 voteCount;
+    }
+
+    enum WorkflowStatus {
+        RegisteringVoters,
+        ProposalsRegistrationStarted,
+        ProposalsRegistrationEnded,
+        VotingSessionStarted,
+        VotingSessionEnded,
+        VotesTallied
+    }
+
+    event VoterRegistered(address voterAddress);
+    event WorkflowStatusChange(WorkflowStatus previousStatus, WorkflowStatus newStatus);
+    event ProposalRegistered(uint256 proposalId);
+    event Voted(address voter, uint256 proposalId);
+}
