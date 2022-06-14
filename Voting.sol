e023dca97f4282bf4c633b63564eac891aaf44a2
Benjamin Reynes <benjamin@morio.co>
2022-06-14 08:56:58 +0200

    Clean + remove voter data duplication

--- Voting.sol
+++ Voting.sol
@@ -1,9 +1,5 @@
 // SPDX-License-Identifier: MIT
-// TODO - Handle equality
-// TODO - essayer de faire des sessions mapping de mapping : Factory ?
-
-// TODO - Option anonyme
-// TODO - Utiliser systeme de role
+// TODO si egalité on revote pour désigner le gagnant
 
 pragma solidity 0.8.14;
 
@@ -11,10 +7,14 @@ import "https://github.com/OpenZeppelin/openzeppelin-contracts/blob/master/contr
 
 /// @author Alyra Student
 /// @title Voting System
-contract Voting is Ownable {
-    uint256[] winningProposalIds;
-    bool isWinnigProposal;
-    bool firstVote;
+contract VotingSystem is Ownable {
+
+    // Struct
+    // Allow to store address while keeping the integrity of Voter
+    struct VoterWrapper {
+        address voterAddress;
+        Voter voterInfo;
+    }
 
     struct Voter {
         bool isRegistered;
@@ -27,13 +27,7 @@ contract Voting is Ownable {
         uint256 voteCount;
     }
 
-    struct Session {
-        uint256[] winningProposalIds;
-        uint256 endOfSession;
-    }
-
-    mapping(uint256 => mapping(uint256 => Proposal)) proposalHistory;
-
+    // Enum to handle different status in the voting workflow
     enum WorkflowStatus {
         RegisteringVoters, // 0
         ProposalsRegistrationStarted, // 1
@@ -43,62 +37,45 @@ contract Voting is Ownable {
         VotesTallied // 5
     }
 
-    Session[] sessionHistory;
-    mapping(address => Voter) public addressToVoter;
-
-    address[] public voters;
+    // Variables
+    // Array of winners (in case of equality)
+    uint256[] winningProposalIds;
+    VoterWrapper[] public voters;
     WorkflowStatus public status;
     Proposal[] proposals;
 
+    // Session
+    struct Session {
+        uint256[] winningProposalIds;
+        uint256 endOfSession;
+    }
+    mapping(uint256 => mapping(uint256 => Proposal)) proposalHistory;
+    Session[] sessionHistory;
+
+    // Events
     event VoterRegistered(address voterAddress);
     event ProposalRegistered(uint256 proposalId);
     event Voted(address voter, uint256 proposalId);
-    event WorkflowStatusChange(
-        WorkflowStatus previousStatus,
-        WorkflowStatus newStatus
-    );
+    event WorkflowStatusChange(WorkflowStatus previousStatus, WorkflowStatus newStatus);
 
-    constructor() {
-        // Allow blank vote
-        proposals.push(Proposal("Vote Null", 0));
-        proposals.push(Proposal("Vote Blanc", 1));
-        //Test
-        //registerVoter(address(0xAb8483F64d9C6d1EcF9b849Ae677dD3315835cb2));
-        //registerVoter(address(0x4B20993Bc481177ec7E8f571ceCaE8A9e22C02db));
-        //registerVoter(address(0x5B38Da6a701c568545dCfcB03FcB875f56beddC4));
-        //registerVoter(address(0x78731D3Ca6b7E34aC0F824c42a7cC18A495cabaB));
+    function isVoterInList(address _address) private view returns (bool inList) {
 
-        //proposals.push(Proposal("Proposal 1", 4));
-        //proposals.push(Proposal("Proposal 1 Bis", 4));
-        //proposals.push(Proposal("Proposal 2", 2));
-        //proposals.push(Proposal("Proposal 3", 1));
-        //proposals.push(Proposal("Proposal 4", 4));
-        //proposals.push(Proposal("Proposal 5", 4));
-        //proposals.push(Proposal("Proposal 6", 1));
-
-        //firstVote = true;
-        //status = WorkflowStatus(5);
-        //calculateWinnner();
-    }
-
-    function isVoterInList(address _address)
-        private
-        view
-        returns (bool inList)
-    {
         for (uint256 i; i < voters.length; i++) {
-            if (voters[i] == _address) {
-                return true;
+            if (voters[i].voterAddress == _address) {
+                if (voters[i].voterInfo.isRegistered) {
+                    return true;
+                }
+                return false;
             }
         }
     }
 
-    modifier voterIsRegistered() {
-        require(isVoterInList(msg.sender), "Voter not registered");
-        require(
-            addressToVoter[msg.sender].isRegistered,
-            "Voter not registered"
-        );
+    // Modifiers
+    modifier voterIsRegistered(uint _voterIndex) {
+        //require(isVoterInList(msg.sender), "Voter not registered");
+        require(_voterIndex < voters.length, "This voter doesn't exist");
+        require(voters[_voterIndex].voterAddress == msg.sender, "You can't vote for someone else");
+
         _;
     }
 
@@ -116,13 +93,12 @@ contract Voting is Ownable {
      * @notice Get the proposal list if the Proposal registration strated
      * @return an Arrays of Proposals or an empty Array
      */
-    function getPoroposals()
-        external
-        view
-        afterStatus(WorkflowStatus.ProposalsRegistrationStarted)
-        returns (Proposal[] memory)
-    {
+    function getPoroposals() external view afterStatus(WorkflowStatus.ProposalsRegistrationStarted) returns (Proposal[] memory) {
         return proposals;
+    }
+
+    function getVoters() external view afterStatus(WorkflowStatus.ProposalsRegistrationStarted) returns (VoterWrapper[] memory) {
+        return voters;
     }
 
     /**
@@ -131,38 +107,28 @@ contract Voting is Ownable {
      * @param index (uint) in the proposals array
      * @return a Proposal object
      */
-    function getProposalById(uint256 index)
-        external
-        view
-        returns (Proposal memory)
-    {
+    function getProposalById(uint256 index) external view returns (Proposal memory) {
         require(index < proposals.length, "Out of range");
         return proposals[index];
     }
 
-    function registerVoter(address _address)
-        public
-        onlyOwner
-        inStatus(WorkflowStatus.RegisteringVoters)
-    {
+    /**
+     * @notice Register a Voter
+     * @dev Register a Voter by his address
+     * @param _address (address)
+     */
+    function registerVoter(address _address) public onlyOwner inStatus(WorkflowStatus.RegisteringVoters) {
         require(_address != address(0));
         require(!isVoterInList(_address), "The Voter is already registered");
 
-        addressToVoter[_address] = Voter(true, false, 0);
-        voters.push(_address);
+        voters.push(VoterWrapper(_address, Voter(true, false, 0)));
         emit VoterRegistered(_address);
     }
 
-    function proposalExist(string calldata _proposalDescription)
-        private
-        view
-        returns (bool)
-    {
+    // Private function that check if a proposal exist
+    function proposalExist(string calldata _proposalDescription) private view returns (bool) {
         for (uint256 i; i < proposals.length; i++) {
-            if (
-                keccak256(abi.encodePacked((proposals[i].description))) ==
-                keccak256(abi.encodePacked((_proposalDescription)))
-            ) {
+            if (keccak256(abi.encodePacked((proposals[i].description))) == keccak256(abi.encodePacked((_proposalDescription)))) {
                 return true;
             }
         }
@@ -175,136 +141,102 @@ contract Voting is Ownable {
         if (status == WorkflowStatus.VotingSessionEnded) {
             calculateWinnner();
         }
+        if (status == WorkflowStatus.RegisteringVoters) {
+            // Allow blank vote
+            proposals.push(Proposal("Vote Null", 0));
+            proposals.push(Proposal("Vote Blanc", 0));
+        }
         WorkflowStatus prevStatus = status;
         status = WorkflowStatus(uint256(status) + 1);
         emit WorkflowStatusChange(prevStatus, status);
     }
 
-    function propose(string calldata _description)
-        external
-        inStatus(WorkflowStatus.ProposalsRegistrationStarted)
-        voterIsRegistered
-    {
-        require(
-            bytes(_description).length > 0,
-            "You have to provide a description"
-        );
+
+    function propose(string calldata _description, uint _voterIndex) external inStatus(WorkflowStatus.ProposalsRegistrationStarted) voterIsRegistered(_voterIndex) {
+        require(bytes(_description).length > 0, "You have to provide a description");
         require(!proposalExist(_description), "This porposal already exist");
 
         proposals.push(Proposal(_description, 0));
         emit ProposalRegistered(proposals.length - 1);
     }
 
-    function vote(uint256 _proposalId)
-        external
-        inStatus(WorkflowStatus.VotingSessionStarted)
-        voterIsRegistered
-    {
-        require(
-            addressToVoter[msg.sender].hasVoted == false,
-            "You already voted"
-        );
+    function vote(uint256 _proposalId, uint256 _voterIndex) external inStatus(WorkflowStatus.VotingSessionStarted) voterIsRegistered(_voterIndex) {
+        require(voters[_voterIndex].voterInfo.hasVoted == false, "You already voted");
         require(_proposalId < proposals.length, "This proposal does not exist");
 
-        addressToVoter[msg.sender].votedProposalId = _proposalId;
-        addressToVoter[msg.sender].hasVoted = true;
+        voters[_voterIndex].voterInfo.votedProposalId = _proposalId;
+        voters[_voterIndex].voterInfo.hasVoted = true;
         proposals[_proposalId].voteCount++;
-        if (!firstVote) {
-            firstVote = true;
-        }
         emit Voted(msg.sender, _proposalId);
     }
 
     function calculateWinnner() private {
         uint256 bestscore;
         uint256[] memory winnerIds = new uint256[](proposals.length);
-
         uint256 counter;
 
         for (uint256 i; i < proposals.length; i++) {
-            if (proposals[i].voteCount == bestscore) {
-                winnerIds[counter] = i;
-                counter++;
-            }
             if (proposals[i].voteCount > bestscore) {
                 counter = 0;
                 winnerIds = new uint256[](proposals.length);
                 winnerIds[counter] = i;
                 bestscore = proposals[i].voteCount;
                 counter++;
+            } else if (proposals[i].voteCount == bestscore) {
+                winnerIds[counter] = i;
+                counter++;
             }
         }
+
+        uint256[] memory tempWinners = new uint256[](counter);
+
+        // Remove zero from Array
         for (uint256 i; i < winnerIds.length; i++) {
             if (winnerIds[i] == 0) {
                 break;
             }
-            winningProposalIds.push(winnerIds[i]);
+            tempWinners[i] = winnerIds[i];
         }
 
         if (winningProposalIds.length == 0) {
-            winningProposalIds.push(0);
+            tempWinners[0] = 0;
         }
 
-        isWinnigProposal = true;
+        winningProposalIds = tempWinners;
     }
 
-    function getWinnerIds()
-        external
-        view
-        inStatus(WorkflowStatus.VotesTallied)
-        returns (uint256[] memory)
-    {
-        require(isWinnigProposal, "The winner has not been chosen yet");
-
+    // Getter to keep winningProposalIds variable private and only access it when status = VotesTallied
+    function getWinnerIds() external view inStatus(WorkflowStatus.VotesTallied) returns (uint256[] memory) {
         return winningProposalIds;
     }
 
-    function OldSessionWinner(uint256 _sessionId)
-        external
-        view
-        returns (Proposal[] memory)
-    {
+    // Get the winners of an old session
+    function OldSessionWinner(uint256 _sessionId) external view returns (Proposal[] memory) {
         require(sessionHistory.length > 0, "No history yet");
-        Proposal[] memory winnerProposals = new Proposal[](
-            sessionHistory[_sessionId].winningProposalIds.length
-        );
 
-        for (
-            uint256 i = 0;
-            i < sessionHistory[_sessionId].winningProposalIds.length;
-            i++
-        ) {
-            winnerProposals[i] = proposalHistory[_sessionId][
-                sessionHistory[_sessionId].winningProposalIds[i]
-            ];
+        uint proposalLenght = sessionHistory[_sessionId].winningProposalIds.length;
+        
+        Proposal[] memory winnerProposals = new Proposal[](proposalLenght);
+
+        for (uint256 i = 0; i < proposalLenght; i++) {
+            winnerProposals[i] = proposalHistory[_sessionId][sessionHistory[_sessionId].winningProposalIds[i]];
         }
         return winnerProposals;
     }
-
-    function resetVotingSystem(bool _saveSession)
-        external
-        onlyOwner
-        inStatus(WorkflowStatus.VotesTallied)
-    {
+    
+    // Reset the votung system and allow to keep history
+    function resetVotingSystem(bool _saveSession) external onlyOwner inStatus(WorkflowStatus.VotesTallied) {
         status = WorkflowStatus(0);
-        // Save in history
+        // Save session in history
         if (_saveSession) {
             sessionHistory.push(Session(winningProposalIds, block.timestamp));
             for (uint256 i = 0; i < proposals.length - 1; i++) {
                 proposalHistory[sessionHistory.length - 1][i] = proposals[i];
-            }
-
-            for (uint256 i = 0; i < voters.length - 1; i++) {
-                addressToVoter[voters[i]] = Voter(false, false, 0);
             }
         }
 
         delete voters;
         delete proposals;
         delete winningProposalIds;
-
-        proposals.push(Proposal("Vote Null", 0));
-        proposals.push(Proposal("Vote Blanc", 0));
-        isWinnigProposal = false;
     }
 }
